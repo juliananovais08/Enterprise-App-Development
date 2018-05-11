b7f52045e9e5f6c0925498b53c713ea229d5c777
logonrm <logonrm@fiap.com.br>
2018-05-11 20:31:51 -0300

    Adicionando projeto 10

--- 09-EJB/src/br/com/fiap/bo/ClienteBO.java
+++ 09-EJB/src/br/com/fiap/bo/ClienteBO.java
@@ -1,8 +1,9 @@
 package br.com.fiap.bo;
 
-import javax.ejb.Local;
 
-@Local
+import javax.ejb.Remote;
+
+@Remote
 public interface ClienteBO {
 	
 	double calcularImpostoRenda(double rendimento);
---
+++ 09-EJB/src/br/com/fiap/bo/impl/ContadorBOImpl.java
@@ -0,0 +1,23 @@
+package br.com.fiap.bo.impl;
+
+import javax.ejb.Singleton;
+import br.com.fiap.bo.ContadorBO;
+
+@Singleton
+public class ContadorBOImpl implements ContadorBO{
+	
+	private int total;
+
+	@Override
+	public void imcrementar() {
+		total++;
+	}
+
+	@Override
+	public int verTotal() {		
+		return total;
+	}
+	
+	
+
+}
